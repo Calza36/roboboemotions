0c619fbc7dfa4754a2ead8e06dedda9026b66457
calza <93159997+Calza36@users.noreply.github.com>
2024-06-11 18:52:14 +0200

    adding emotion image

---
+++ robobo_emotion/emotion_image.py
@@ -0,0 +1,20 @@
+from deepface import DeepFace
+
+class EmotionImage:
+    def __init__(self, image_path):
+        self.image_path = image_path
+        self.emotion = self.get_emotion()
+
+    def get_emotion(self):
+        result = DeepFace.analyze(self.image_path)
+        emotion = result[0]['dominant_emotion']
+        return emotion
+
+    def get_image_path(self):
+        return self.image_path
+
+    def get_emotion(self):
+        return self.emotion
+
+
+
