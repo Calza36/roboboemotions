468bdebac0f76ac933f5e8404c7afe264a4ed086
calza <93159997+Calza36@users.noreply.github.com>
2024-06-11 19:16:02 +0200

    adding setup url

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='robobo_emotion',
     version='0.1',
+    repository_url="https://github.com/Calza36/roboboemotions.git",
     packages=find_packages(),
     install_requires=[
         'robobopy',
