c53c7a5496a433eb91d065bca8aab71e2f469008
calza <93159997+Calza36@users.noreply.github.com>
2024-06-11 19:25:17 +0200

    adding setup

--- setup.py
+++ setup.py
@@ -4,16 +4,13 @@ setup(
     name='robobo_emotion',
     version='0.1',
     repository_url="https://github.com/Calza36/roboboemotions",
+    py_modules=['robobo_emotion'],
     packages=find_packages(),
     install_requires=[
         'robobopy',
         'robobopy_videostream',
         'deepface',
         'emotion2vec',
-    ],
-    entry_points={
-        'console_scripts': [
-            'robobo_emotion=robobo_emotion:main',
-        ],
-    },
+    ]
+    
 )
